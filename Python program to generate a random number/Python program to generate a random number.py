8c97d6d1269bb2804906a3155bb3c2f867bd5b99
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2020-05-01 01:53:50 +0300

    Generate a random number (given range by user)

---
+++ Python program to generate a random number/Python program to generate a random number.py
@@ -0,0 +1,11 @@
+#By Daliah Aljutayli
+#Python program to generate a random number
+
+import random
+
+print('Enter the range:')
+
+start = int(input('From: '))
+end =  int(input('To: '))
+
+print('Random Number= ',random.randint(start,end)) #int number
